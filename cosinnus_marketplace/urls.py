7f6fffb1d1a7d2209d81df55db12b595677b2356
sascha <sn@sinnwerkstatt.com>
2018-11-13 16:10:03 +0100

    Django 2.0 compatibility

--- cosinnus_marketplace/urls.py
+++ cosinnus_marketplace/urls.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.conf.urls import url
 from cosinnus_marketplace import views
 
+app_name = 'marketplace'
+
 cosinnus_group_patterns = [
     url(r'^$', views.index_view, name='index'),
 
