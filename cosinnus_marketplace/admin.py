bc1a212e21e00bdd99ad0eb682ee7b5acbeefc72
sascha <sn@sinnwerkstatt.com>
2019-01-30 16:54:01 +0100

    Lots of cleanup and improvements for admin views

--- cosinnus_marketplace/admin.py
+++ cosinnus_marketplace/admin.py
@@ -4,12 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from cosinnus_marketplace.models import Offer, OfferCategory, OfferCategoryGroup
+from cosinnus.admin import BaseTaggableAdminMixin
 
 
-class OfferAdmin(admin.ModelAdmin):
-    list_display = ('title', 'type', 'creator', 'group',)
-    list_filter = ('type', 'creator', 'group',)
-    search_fields = ('title', 'description', 'user__first_name', 'user__last_name', 'user__email', 'group__name')
+class OfferAdmin(BaseTaggableAdminMixin, admin.ModelAdmin):
+    list_display = BaseTaggableAdminMixin.list_display + ['is_active', 'type',]
+    list_filter = BaseTaggableAdminMixin.list_filter + ['is_active', 'type',]
 
 admin.site.register(Offer, OfferAdmin)
 
